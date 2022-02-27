a244e385bd77ea28aac57657659b5b259261494a
Edwin Jose George <edwinjosegeorge@gmail.com>
2022-02-27 19:30:22 +0530

    Display role in hierarchy

--- company.py
+++ company.py
@@ -9,15 +9,21 @@ while True:
     try:
         print("\nOperations :")
         print("\t1. Add Sub Role")
-        print("\t2. Exit")
+        print("\t2. DisplayRoles")
+        print("\t3. Exit")
         operation = int(input("Operation to be performed : "))
-        if operation == 2:
+        if operation == 3:
             break
 
         if operation == 1:  # add sub role
             child_name = input("Enter sub role name : ")
             parent_name = input("Enter reporting to role name : ")
             ROOT.addSub(parent_name, child_name)
+
+        if operation == 2:
+            for role in ROOT:
+                print(role, end=" ")
+            print()
 
     except Exception as e:
         print(f"Error: {e}")
