bcae46dc2b9193a09d5f52cbe90954e34070a288
Edwin Jose George <edwinjosegeorge@gmail.com>
2022-02-27 19:12:29 +0530

    Add sub role operation

--- company.py
+++ company.py
@@ -2,5 +2,22 @@ from role import ROLE
 
 root_name = input("Enter root role name : ")
 ROOT = ROLE(root_name)
-
 print(ROOT)
+
+operation = 0
+while True:
+    try:
+        print("\nOperations :")
+        print("\t1. Add Sub Role")
+        print("\t2. Exit")
+        operation = int(input("Operation to be performed : "))
+        if operation == 2:
+            break
+
+        if operation == 1:  # add sub role
+            child_name = input("Enter sub role name : ")
+            parent_name = input("Enter reporting to role name : ")
+            ROOT.addSub(parent_name, child_name)
+
+    except Exception as e:
+        print(f"Error: {e}")
--- role.py
+++ role.py
@@ -1,3 +1,20 @@
+class ROLE_ITERATE:
+    """
+    Iterable version of class ROLE
+    """
+
+    def __init__(self, RoleObj):
+        self.lst = [RoleObj]
+
+    def __next__(self):
+        if len(self.lst) == 0:
+            raise StopIteration
+        nxt = self.lst[0]
+        self.lst.remove(nxt)
+        self.lst.extend(nxt.child)
+        return nxt
+
+
 class ROLE:
     """
     Class for handling roles
@@ -6,8 +23,31 @@ class ROLE:
     def __init__(self, name: str):
         """Constructor to initialize role name"""
         self.name = name
-        self.__parent = None  # private member
-        self.__child = list()  # private member
+        self.child = list()
 
     def __str__(self) -> str:
         return self.name
+
+    def __iter__(self):
+        return ROLE_ITERATE(self)
+
+    def findRole(self, name: str):
+        """Search and return role object identified by name in sub tree"""
+        for role in self:
+            if role.name == name:
+                return role
+        return None
+
+    def addSub(self, parent_name: str, child_name: str) -> None:
+        """Add new sub role to parent in this subtree. Creates new child"""
+
+        parent = self.findRole(parent_name)
+        child = self.findRole(child_name)
+        if parent is None:
+            raise ValueError(f"No role {parent_name} found")
+        if child is not None:
+            raise ValueError(f"Role {child_name} pre-exist")
+
+        child = ROLE(child_name)
+        child.parent = parent
+        parent.child.append(child)
